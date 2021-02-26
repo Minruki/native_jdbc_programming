6c751418682c1a58027053152f8e56ce05d70091
Min <rukiminangel@naver.com>
2021-02-26 12:19:40 +0900

    수정

--- Scripts/test.sql
+++ Scripts/test.sql
@@ -6,43 +6,54 @@ desc title;
 desc department;
 desc employee;
 
-select * from employee;
-insert into employee values (1004, '이민정', 3, 4377, 3000000, 3);
-update employee set empname = '민루키' where empno = 1004;
 select tno, tname from title;
-select * from department;
-select tno, tname from title t where tno = 3;
+select deptNo, deptName, floor from department;
 
-update title set tname = '계약직' where tno = 6;
+select tno, tname from title where tno = 3;
+
+update title set tname='계약직' where tno = 6;
 
 delete from title where tno = 6;
 delete from department where deptno = 5;
 
 insert into title values(6, '인턴');
-insert 
 
 select deptNo, deptName, floor from department;
-select empno, empname, title, manager, salary, dept from employee;
 
-select e.*, t.*, m.emp_no, m.emp_name, d.*
-  from employee e join title t on e.tno = t.title_no
-  		left join employee e on e.manager = m.emp_no
-  		join department d on e.dno=d.dpet_no;
-
-  	
-create or replace view vw_employee
-(empno, empname, tno, titlename, manager, mgrName, salary, dno, deptname, floor)
-as 
-select e.empname, e.empname, e.tno, t.titlename, e.salary,
-	   d.deptno, d.deptname, d.floor
-  from employee e join title t on e.tno = t.titleno
-	   left join employee m on e.manager = m.empno
-	   join department d on e.dno = d.deptno;
-	  
-select emp_no, emp_name, manager, salary, mgr_name, tno, title_name, dno, dept_name, floor
-  from vw_employee;
+create or replace view vw_full_employee
+as
+select e.empno
+     , e.empname
+     , t.tno as title_no
+     , t.tname as title_name
+     , e.manager as manager_no
+     , m.empname as manager_name
+     , e.salary
+     , d.deptNo
+     , d.deptName
+     , d.floor
+  from employee e join title t on e.title = t.tno
+       left join employee m on e.manager = m.empno 
+       join department d on e.dept = d.deptNo ;
   
-select emp_no, emp_name, manager, salary, mgr_name, tno, title_name, dno, dept_name, floor
-  from vw_employee
- where emp_no = 1003;
+select * from vw_full_employee;
+
+select empno, empname, title_no, title_name, manager_no, manager_name, salary, deptNo, deptName, floor
+  from vw_full_employee;
  
+select empno, empname, title as title_no, manager as manager_no, salary, dept as deptNo
+  from employee;
+ 
+ 
+insert into employee values(1004, '천사', 5, 4377, 2000000, 1);
+update employee 
+   set dept = 3
+ where empno = 1004;
+delete 
+  from employee 
+ where empno = 1004;
+ 
+-- 부서가 1인 사원정보를 출력
+select empno, empname, title, manager, salary, dept
+  from employee 
+ where dept = (select deptNo from department where deptNo = 3);
--- test/native_jdbc_programming/dao/EmployeeDaoTest.java
+++ test/native_jdbc_programming/dao/EmployeeDaoTest.java
@@ -9,16 +9,14 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import native_jdbc_programming.dao.impl.EmployeeDaoImpl;
-import native_jdbc_programming.dao.impl.TitleDaoImpl;
 import native_jdbc_programming.dto.Department;
 import native_jdbc_programming.dto.Employee;
 import native_jdbc_programming.dto.Title;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-
 public class EmployeeDaoTest {
-	private static EmployeeDao dao = EmployeeDaoImpl.getInstance();
-
+	private EmployeeDao dao = EmployeeDaoImpl.getInstance();
+	
 	@After
 	public void tearDown() throws Exception {
 		System.out.println();
@@ -27,48 +25,50 @@ public class EmployeeDaoTest {
 	@Test
 	public void test01SelectEmployeeByAll() {
 		System.out.printf("%s()%n", "testSelectEmployeeByAll");
-		List<Employee> employeeList = dao.selectEmployeeByAll();
-		Assert.assertNotNull(employeeList);
+		List<Employee> empList = dao.selectEmployeeByAll();
+		Assert.assertNotNull(empList);
 		
-		for(Employee t : employeeList) {
+//		titleList.stream().forEach(System.out::println);
+		for(Employee t : empList) {
 			System.out.println(t);
 		}
-		
 	}
 
 	@Test
 	public void test02SelectEmployeeByNo() {
-		Employee employee = new Employee(3427);
-		Employee searchEmployee = dao.selectEmployeeByNo(employee);
-		Assert.assertNotNull(searchEmployee);
-		System.out.println(dao.selectEmployeeByNo(employee));
+		System.out.printf("%s()%n", "testSelectEmployeeByNo");
+		Employee selEmp = new Employee(2106);
+		
+		Employee emp = dao.selectEmployeeByNo(selEmp);
+		Assert.assertNotNull(emp);
+		System.out.println(emp);
 	}
 
 	@Test
 	public void test03InsertEmployee() {
 		System.out.printf("%s()%n", "testInsertEmployee");
-		Employee newEmployee = new Employee(1004, "천사", new Title(3), new Employee(4377), 3000000, new Department(3));
-		int res = dao.insertEmployee(newEmployee);
+		Employee newEmp = new Employee(1004, "천사", new Title(5), new Employee(4377), 2000000, new Department(1));
+		int res = dao.insertEmployee(newEmp);
 		Assert.assertEquals(1, res);
-		System.out.println(dao.selectEmployeeByNo(newEmployee));
+		
+		System.out.println(dao.selectEmployeeByNo(newEmp));
 	}
 
 	@Test
 	public void test04UpdateEmployee() {
 		System.out.printf("%s()%n", "testUpdateEmployee");
-		Employee newEmployee = new Employee(1004, "천사2", new Title(3), new Employee(4377), 3000000, new Department(3));
-		//update employee set empname = 1004, title_no = '천사', salary =3, dept =1003 where empno = 3000000
-		
-		int res = dao.updateEmployee(newEmployee);
+		Employee newEmp = new Employee(1004, "천사2", new Title(4), new Employee(1003), 2000000, new Department(2));
+		int res = dao.updateEmployee(newEmp);
 		Assert.assertEquals(1, res);
 		
+		System.out.println(dao.selectEmployeeByNo(newEmp));
 	}
 
 	@Test
 	public void test05DeleteEmployee() {
-		System.out.printf("%s()%n", "testDeletEmployee");
-		Employee newEmployee = new Employee(1004);
-		int res = dao.deleteEmployee(newEmployee);
+		System.out.printf("%s()%n", "testDeleteEmployee");
+		Employee newEmp = new Employee(1004);
+		int res = dao.deleteEmployee(newEmp);
 		Assert.assertEquals(1, res);
 		dao.selectEmployeeByAll().stream().forEach(System.out::println);
 	}
